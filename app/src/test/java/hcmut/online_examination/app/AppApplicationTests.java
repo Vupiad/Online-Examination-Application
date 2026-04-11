3bf54f24ed1548d977c7e822e071190e7b1986ff
Vupiad <12345vupham@gmail.com>
2026-04-11 10:48:50 +0700

    add front-end directory

---
+++ app/src/test/java/hcmut/online_examination/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmut.online_examination.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
