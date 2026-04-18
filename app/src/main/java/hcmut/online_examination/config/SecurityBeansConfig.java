f70c7e764bd955eea89fa9ef12a77a80ae8acd0b
Vupiad <12345vupham@gmail.com>
2026-04-18 08:51:59 +0700

    local change

--- app/src/main/java/hcmut/online_examination/config/SecurityBeansConfig.java
+++ app/src/main/java/hcmut/online_examination/config/SecurityBeansConfig.java
@@ -1,15 +1,15 @@
-// package hcmut.online_examination.config;
+ package hcmut.online_examination.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
+ import org.springframework.context.annotation.Bean;
+ import org.springframework.context.annotation.Configuration;
+ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+ import org.springframework.security.crypto.password.PasswordEncoder;
 
-// @Configuration
-// public class SecurityBeansConfig {
+ @Configuration
+ public class SecurityBeansConfig {
 
-//     @Bean
-//     public PasswordEncoder passwordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
-// }
+     @Bean
+     public PasswordEncoder passwordEncoder() {
+         return new BCryptPasswordEncoder();
+     }
+ }
--- app/src/main/java/hcmut/online_examination/config/SecurityConfig.java
+++ app/src/main/java/hcmut/online_examination/config/SecurityConfig.java
@@ -1,15 +1,15 @@
-// package hcmut.online_examination.config;
+ package hcmut.online_examination.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.Customizer;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-// import org.springframework.security.web.SecurityFilterChain;
-// import jakarta.servlet.DispatcherType;
+ import org.springframework.context.annotation.Bean;
+ import org.springframework.context.annotation.Configuration;
+ import org.springframework.security.config.Customizer;
+ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+ import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+ import org.springframework.security.web.SecurityFilterChain;
+ import jakarta.servlet.DispatcherType;
 
-// @Configuration
-// public class SecurityConfig {
+ @Configuration
+ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
